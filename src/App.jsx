7ca1e86880132c39e442c5ae6e376c72529601c5
Noelia <noeliairanzoribera@gmail.com>
2023-12-13 15:43:02 +0100

    iterations 1-3

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
 
+import { Routes, Route } from "react-router-dom"; 
 
 function App() {
 
@@ -12,11 +13,12 @@ function App() {
       <Navbar />
 
       <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/students/:studentId" element={<StudentDetailsPage/>} />
+        <Route path="/profile" element={<UserProfilePage/>} />
+        <Route path="*" element={<h1>404 Page</h1>} />
+      </Routes>
       </div>
 
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import StudentCard from "../components/StudentCard";
+import { Link } from "react-router-dom"; 
 
 import studentsData from "../assets/students.json";
 
@@ -21,7 +22,7 @@ function HomePage() {
       {students &&
         students.map((student) => {
           return (
-              <StudentCard key={student._id} {...student} />
+              <Link to={`/students/${student._id}`}><StudentCard key={student._id} {...student} /></Link>
           );
         })}
     </div>
