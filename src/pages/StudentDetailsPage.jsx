e7d6d45b305b93db37ac52d36caa15ba1e3daec8
Noelia <noeliairanzoribera@gmail.com>
2023-12-14 10:05:58 +0100

    iteration 4 added

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -7,8 +7,13 @@ function StudentDetailsPage() {
   // Find the current student profile by id.
   // In this case, the student with id 1. The `studentId` is hard-coded for now.
   // This could be a URL parameter from React Router, e.g. /students/:studentId
-  const studentId = "1";
+  const { studentId } = useParams();
   const studentProfile = studentsData.find((student) => student._id === studentId);
+
+  // if no student is found then it goes back to Home.
+  if (!studentProfile) {
+    return <Navigate to ="/" />
+  }
 
   return (
     <div className="StudentDetailsPage bg-gray-100 py-6 px-4 border-2 border-fuchsia-500 m-2">
